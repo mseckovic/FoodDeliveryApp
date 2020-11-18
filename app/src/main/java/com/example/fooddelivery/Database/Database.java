eadf8ecfe7d91a6b36aa684c3901893ff61551eb
Milos Seckovic <milossec@gmail.com>
2020-11-18 12:47:29 +0100

    Bug fix
    - Fixed bug with Cart

--- app/src/main/java/com/example/fooddelivery/Database/Database.java
+++ app/src/main/java/com/example/fooddelivery/Database/Database.java
@@ -53,7 +53,7 @@ public class Database extends SQLiteAssetHelper {
     public void addToCart(Order order)
     {
         SQLiteDatabase db = getReadableDatabase();
-        String query = String.format("INSERT INTO OrderDetail(ProductId,ProductName,Quantity,Price,Discount) VALUES ('%s', '%s', '%s'. '%s', '%s');",
+        String query = String.format("INSERT INTO OrderDetail(ProductId,ProductName,Quantity,Price,Discount) VALUES ('%s', '%s', '%s', '%s', '%s');",
                 order.getProductId(),
                 order.getProductName(),
                 order.getQuantity(),
